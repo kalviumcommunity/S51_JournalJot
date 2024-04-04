307b40e8f0682a8507a49b9cd20d625d17b68152
Premapriya1905 <Premapriya1905@gmail.com>
2024-04-04 15:10:57 +0530

    linking-signup-&-loginpage

--- my-react/src/App.jsx
+++ my-react/src/App.jsx
@@ -1,5 +1,7 @@
 import Login from './Components/Login.jsx'
 import Signup from './Components/Signup.jsx'
+import { Link } from 'react-router-dom';
+import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
 import './App.css'
 
 
@@ -7,7 +9,13 @@ function App() {
 
   return (
     <>
-    <Login/>
+    <Router>
+      <Routes>
+      <Route path="/login" element={<Login />} /> 
+        <Route path="/signup" element={<Signup />} /> 
+      </Routes>
+         
+    </Router>
     </>
   )
 }
