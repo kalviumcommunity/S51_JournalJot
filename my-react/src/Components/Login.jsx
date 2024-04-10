65af15529881b11a7ed86ba325f97655b4736815
Premapriya1905 <Premapriya1905@gmail.com>
2024-04-10 16:23:06 +0530

    login

--- my-react/src/Components/Login.jsx
+++ my-react/src/Components/Login.jsx
@@ -1,23 +1,46 @@
-import React from 'react'
-import '../Components/Login.css'
+import React, { useState } from 'react';
+import '../Components/Login.css';
 import { Link } from 'react-router-dom';
 
 function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Handle login logic here
+  };
+
   return (
-    <div>
-      <h1 id='head'>Login</h1>
-        <div className='login-div'>
-            <h3 id='name1'>UserName </h3><input id='input1' type="text" />
-            <h3 id='email1'>Email </h3><input id='input2' type="text" />
-            <h3 id='pass1'>Password </h3><input id='input3' type="text" />
-            <div><button id='reset'>Reset Password</button><button id='forgot'>Forgot Password</button></div>
-            <Link to='/main'>
-              <button id='enter2'>Enter</button>
-            </Link>
-            
+    <>
+      <div className='login-page'>
+        <div id='login-image'></div>
+        <div id='login-form'>
+          <h2>Login</h2>
+          <form onSubmit={handleSubmit}>
+            <div className="input-group">
+              <label htmlFor="email">Email:</label>
+              <input type="email" id="email" value={email} onChange={handleEmailChange} required />
+            </div>
+            <div className="input-group">
+              <label htmlFor="password">Password:</label>
+              <input type="password" id="password" value={password} onChange={handlePasswordChange} required />
+            </div>
+            <button type="submit">Login</button>
+          </form>
+          <p>Don't have an account? <Link to="/signup">Sign up</Link></p>
         </div>
-    </div>
-  )
+      </div>
+    </>
+  );
 }
 
-export default Login
+export default Login;
