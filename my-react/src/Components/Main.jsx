95ca033233b947873bc73f2c85ae9a684af0bf69
Premapriya1905 <Premapriya1905@gmail.com>
2024-04-14 20:28:50 +0530

    importing-react-calendar

--- my-react/src/Components/Main.jsx
+++ my-react/src/Components/Main.jsx
@@ -1,50 +1,16 @@
-// import React from 'react'
-// import menu from "../assets/menu.png"
-// import bullet from '../assets/bullet.jpg'
-// import number from '../assets/number.png'
-// import divider from '../assets/divider.png'
-// import embed from '../assets/embed.png'
-// import '../Components/Main.css'
-
-// function Main() {
-//   return (
-//     <>
-//     <div className='text-box'>
-//         <div id='main-div'>
-//             <button id='publish'>Publish</button><br />
-//             <div>
-//               <button id='new-note'>➕</button>
-//               <img src={menu} alt="Image" className="menu2"/>
-//             </div>
-//         </div>
-//         <div className='icons'>
-//             <button id='bold'>B</button>
-//             <button id='italic'>I</button>
-//             <img src={bullet} alt="Image" id='bullet'/>
-//             <img src={number} alt="Image" id='number'/>
-//             <img src={number} alt="Image" id='divider'/>
-//             <button id='attach'>🔗</button>
-//             <img src={embed} alt="Image" id='embed'/>
-//         </div>
-//     </div>
-//     </>
-    
-    
-//   )
-// }
-
-// export default Main
-
-
-import React from 'react';
+import React , {useState} from 'react';
 import menu from "../assets/menu.png";
 import bullet from '../assets/bullet.jpg';
 import number from '../assets/number.webp';
 import divider from '../assets/divider.png';
 import embed from '../assets/embed.png';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css'; // Import Calendar CSS
 import '../Components/Main.css';
 
 function Main() {
+  const [value, onChange] = useState(new Date()); // State for the calendar value
+
   return (
     <div className='text-box'>
       <div className="navbar">
@@ -60,8 +26,9 @@ function Main() {
           <img src={embed} alt="Image" id='embed'/>
         </div>
       </div>
-      {/* <div className="text-area">
-      </div> */}
+      <div className='calendar-container'> 
+          <Calendar onChange={onChange} value={value} /> 
+      </div>
     </div>
   );
 }
