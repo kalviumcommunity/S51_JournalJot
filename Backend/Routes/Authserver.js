de616988e798047484ea60a04a05616874c863ae
Premapriya1905 <Premapriya1905@gmail.com>
2024-04-26 16:13:55 +0530

    creating-model

--- Backend/Routes/Authserver.js
+++ Backend/Routes/Authserver.js
@@ -1,5 +1,5 @@
 const express = require('express')
-require('dotenv').config   
+require('dotenv').config()  
 const jwt = require('jsonwebtoken')
 const bcrypt = require('bcrypt')
 const signup =express.Router();
