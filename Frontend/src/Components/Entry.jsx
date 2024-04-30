9494435a485b06938c932ef1a44c0a4cbf3950ff
Premapriya1905 <Premapriya1905@gmail.com>
2024-04-30 16:51:16 +0530

    adding-text-editor

--- Frontend/src/Components/Entry.jsx
+++ Frontend/src/Components/Entry.jsx
@@ -1,23 +1,50 @@
-import React , {useState} from 'react'
-import Navbar from './Navbar'
-import "./Entry.css";
+import React, { useState, useEffect } from 'react';
+import Navbar from './Navbar';
+import './Entry.css';
 import { Calendar } from 'primereact/calendar';
-        
+import { Editor } from 'react-draft-wysiwyg';
+import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
 
 function Entry() {
-
   const [date, setDate] = useState(null);
+  const [editorState, setEditorState] = useState(null);
+
+  useEffect(() => {
+    const loadEditor = async () => {
+      const { EditorState } = await import('draft-js'); // Import EditorState dynamically
+      setEditorState(EditorState.createEmpty());
+    };
+    loadEditor();
+  }, []);
+
+  const onEditorStateChange = (editorState) => {
+    setEditorState(editorState);
+  };
+
   return (
     <>
-      <Navbar/>
-      <div className='content'>
-        <div className='calendar'>
-        Date:<Calendar className='calen' value={date} onChange={(e) => setDate(e.value)} dateFormat="dd/mm/yy" />
+      <Navbar />
+      <div className="content">
+        <div className="calendar">
+          <div>
+            Date:
+            <Calendar className="calen" value={date} onChange={(e) => setDate(e.value)} dateFormat="dd/mm/yy" />
+          </div>
+          
+          <button className='save'>Save</button>
         </div>
-        {/* <div className='title'>Enter Title...</div>     */}
+        
+        <Editor
+          editorState={editorState}
+          toolbarClassName="toolbarClassName"
+          wrapperClassName="wrapperClassName"
+          editorClassName="editorClassName"
+          onEditorStateChange={onEditorStateChange} // Pass the function to handle editor state changes
+        />
+        <div className="title">Enter Title...</div>
       </div>
     </>
-  )
+  );
 }
 
-export default Entry
+export default Entry;
