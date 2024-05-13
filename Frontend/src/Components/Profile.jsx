b9a04d730581ebac6e0812a94203356835574358
Premapriya1905 <Premapriya1905@gmail.com>
2024-05-13 15:07:52 +0530

    bug-fixing

--- Frontend/src/Components/Profile.jsx
+++ Frontend/src/Components/Profile.jsx
@@ -1,6 +1,7 @@
 import React, {useState} from 'react'
 import './Profile.css'
-import { Link ,Navigate,useNavigate} from 'react-router-dom';
+import axios from 'axios'
+import { Link ,useNavigate} from 'react-router-dom';
 
 function Profile() {
 
@@ -8,7 +9,7 @@ function Profile() {
     const [nickname, setNickName]= useState('');
     const [hobbies, setHobbies]=useState('');
     const [description, setdescription]=useState('');
-    
+    const navigate = useNavigate()
     function getCookie(name) {
         let cookieArray = document.cookie.split('; ');
         let cookie = cookieArray.find((row) => row.startsWith(name + '='));
@@ -34,14 +35,19 @@ function Profile() {
 
     const submit = (e)=>{
         e.preventDefault();
-        axios.post('',{
-            ProfileName:profileName,
+        axios.post('http://localhost:3000/api/addprofile',{
+            Profilename:profileName,
             Nickname:nickname,
             Hobbies:hobbies,
             Description:description
-        },{headers:{authorization:`Beaare ${token}`}}).then((response)=>{
+        },{headers:{authorization:`Beaare ${getCookie('token')}`}}).then((response)=>{
             console.log(response.data);
-            Navigate('/')
+            setCookie("ProfileName",profileName,365);
+            setCookie("Nickname", nickname, 365);
+            setCookie("Hobbies", hobbies, 365);
+            setCookie("Description", description, 365)
+            setCookie("ID",response.data._id)
+            navigate('/')
         }).catch((error) => console.error(error))
     }
 
@@ -54,22 +60,25 @@ function Profile() {
         <div className='inputs'>
             <form onSubmit={submit}>
             <div className='input'>
-                <input type="text" onClick={handleProfileNameChange} placeholder='Name...' />
+                <input type="text" onChange={handleProfileNameChange} placeholder='Name...' />
             </div>
             <div className='input'>
-                <input type="text" onClick={handleNickNameChange} placeholder='Nick Name...' />
+                <input type="text" onChange={handleNickNameChange} placeholder='Nick Name...' />
             </div>
             <div className='input'>
-                <input type="text" onClick={handleHobbiesChange} placeholder='Hobbies...' />
+                <input type="text" onChange={handleHobbiesChange} placeholder='Hobbies...' />
             </div>
             <div className='input'>
-                <input className='brief' onClick={handleDescriptionChange} type="text" placeholder='Brief description on yourself...' />
+                <input className='brief' onChange={handleDescriptionChange} type="text" placeholder='Brief description on yourself...' />
             </div>
-            {/* <Link to='/login'> */}
-            <button className='Save'>Save</button>
-            {/* </Link> */}
+            <Link to='/login'>
+            <button type='submit' className='Save'>Save</button>
+            </Link>
             </form>
         </div>
+
+        
+
         </div>
     </div>
 </div>
